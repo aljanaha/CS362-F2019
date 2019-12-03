739205f42b12920e9e8269033afcf099457aef88
Abdulaziz Al-Janahi <aljanaha@oregonstate.edu>
2019-12-03 13:32:59 -0500

    test1

---
+++ projects/FinalProject-Bugs/dominion/unittest_helpers.h
@@ -0,0 +1,11 @@
+#ifndef _UNITTEST_HELPERS_H
+#define _UNITTEST_HELPERS_H
+
+void setPlayerHand(int p, int handCount, int *hand, struct gameState *G);
+void assertOne(int expected, char *messag);
+void assert(int expected, int actual, char *message);
+int compareArrays(int *array1, int *array2, int size1, int size2);
+int unchangedPlayer(int player, struct gameState *stateAfter, struct gameState *stateBefore);
+int nextTurn(struct gameState *G);
+
+#endif
