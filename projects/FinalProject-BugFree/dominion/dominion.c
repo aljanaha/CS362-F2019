1956e950e4737666ba4555c942c0c34f1c7b1fc0
zee100100 <>
2019-12-03 20:44:51 -0500

    fix bug 5

--- projects/FinalProject-BugFree/dominion/dominion.c
+++ projects/FinalProject-BugFree/dominion/dominion.c
@@ -432,7 +432,7 @@ int isGameOver(struct gameState *state)
 
     //if three supply pile are at 0, the game ends
     j = 0;
-    for (i = 0; i < 25; i++)
+    for (i = curse; i <= treasure_map; i++)
     {
         if (state->supplyCount[i] == 0)
         {
